bd85dab7c93c7331c7bd29cdcdca924407f3a02a
saneness <saneness@protonmail.com>
2025-09-29 09:02:06 +0100

    add try/catch for expired requests

--- pots.py
+++ pots.py
@@ -27,25 +27,30 @@ def send_message(text):
 if __name__ == '__main__':
     args = args()
     
-    ACCESS_TOKEN = open('.access_token').read()
-    ACCOUNT_ID   = open('.account_id').read()
+    ACCESS_TOKEN = open('.access_token').read().strip()
+    ACCOUNT_ID   = open('.account_id').read().strip()
 
     headers = {'Authorization': ACCESS_TOKEN}
     request = requests.get(f'https://api.monzo.com/pots?current_account_id={ACCOUNT_ID}', headers=headers)
-    balance = int([item for item in request.json()['pots'] if item['has_virtual_cards']][0]['balance'])/100.0
-    if args.convert in ["JPY"]:
-        converted = int(round(CurrencyConverter().convert(balance, 'GBP', f'{args.convert}'), 0))
-    else:
-        converted = round(CurrencyConverter().convert(balance, 'GBP', f'{args.convert}'), 2)
-    new_balance_gbp = f'{balance}'
-    new_balance_con = f'{converted}'
-
     try:
-        old_balance_gbp = open(POT_PATH).read().strip().split()[0]
-        old_balance_con = open(POT_PATH).read().strip().split()[2][1:]
-        if new_balance_gbp != old_balance_gbp:
-            open(POT_PATH, 'w+').write(f'{new_balance_gbp} GBP ({new_balance_con} {args.convert})')
-            send_message(f"`GBP: {old_balance_gbp} -> {new_balance_gbp}`\n`{args.convert}: {old_balance_con} -> {new_balance_con}`")
+        balance = int([item for item in request.json()['pots'] if item['has_virtual_cards']][0]['balance'])/100.0
+        if os.path.exists(POT_PATH_ERROR):
+            os.remove(POT_PATH_ERROR)
+        if args.convert in ["JPY"]:
+            converted = int(round(CurrencyConverter().convert(balance, 'GBP', f'{args.convert}'), 0))
+        else:
+            converted = round(CurrencyConverter().convert(balance, 'GBP', f'{args.convert}'), 2)
+        new_balance_gbp = f'{balance}'
+        new_balance_con = f'{converted}'
+
+        try:
+            old_balance_gbp = open(POT_PATH).read().strip().split()[0]
+            old_balance_con = open(POT_PATH).read().strip().split()[2][1:]
+            if new_balance_gbp != old_balance_gbp:
+                open(POT_PATH, 'w+').write(f'{new_balance_gbp} GBP ({new_balance_con} {args.convert})')
+                send_message(f"`GBP: {old_balance_gbp} -> {new_balance_gbp}`\n`{args.convert}: {old_balance_con} -> {new_balance_con}`")
+        except:
+            new_balance = f'{new_balance_gbp} GBP ({new_balance_con} {args.convert})'
+            open(POT_PATH, 'w+').write(new_balance)
     except:
-        new_balance = f'{new_balance_gbp} GBP ({new_balance_con} {args.convert})'
-        open(POT_PATH, 'w+').write(new_balance)
+        open(POT_PATH_ERROR, 'w+').write('ERROR')
