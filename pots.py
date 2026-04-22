f405bb814ba00134ed15f5a63011040b34cfcbf1
saneness <saneness@protonmail.com>
2026-04-22 11:17:35 +0100

    update conversion method

--- pots.py
+++ pots.py
@@ -8,7 +8,7 @@ import json
 import os
 import time
 import traceback
-from currency_converter import CurrencyConverter
+from forex_python.converter import CurrencyRates
 
 from config import *
 
@@ -49,22 +49,26 @@ if __name__ == '__main__':
         balance = int([item for item in request.json()['pots'] if item['has_virtual_cards']][0]['balance'])/100.0
         if os.path.exists(POT_PATH_ERROR):
             os.remove(POT_PATH_ERROR)
-        if args.convert in ["JPY"]:
-            converted = int(round(CurrencyConverter().convert(balance, 'GBP', f'{args.convert}'), 0))
+        rates = CurrencyRates()
+        converted = rates.convert('GBP', f'{args.convert}', balance)
+        if args.convert in ['JPY']:
+            converted = int(round(converted, 0))
         else:
-            converted = round(CurrencyConverter().convert(balance, 'GBP', f'{args.convert}'), 2)
+            converted = round(converted, 2)
         new_balance_gbp = f'{balance}'
         new_balance_con = f'{converted}'
 
         try:
             old_balance_gbp = open(POT_PATH).read().strip().split()[0]
-            old_balance_con = open(POT_PATH).read().strip().split()[2][1:]
             if new_balance_gbp != old_balance_gbp:
                 balance_change_gbp = float(new_balance_gbp)-float(old_balance_gbp)
-                balance_change_con = float(new_balance_con)-float(old_balance_con)
+                balance_change_con = rates.convert('GBP', f'{args.convert}', balance_change_gbp)
                 balance_change_sign_gbp = '+' if balance_change_gbp > 0 else '-'
                 balance_change_sign_con = '+' if balance_change_con > 0 else '-'
-                send_message(f"`New balance:`\n`{SYMBOL['GBP']}{new_balance_gbp} ({balance_change_sign_gbp}{SYMBOL['GBP']}{abs(balance_change_gbp):{FORMAT['GBP']}})`\n`{SYMBOL[args.convert]}{new_balance_con} ({balance_change_sign_con}{SYMBOL[args.convert]}{abs(balance_change_con):{FORMAT[args.convert]}})`")
+                send_message(
+                    f"`New balance:`\n`{SYMBOL['GBP']}{new_balance_gbp} ({balance_change_sign_gbp}{SYMBOL['GBP']}{abs(balance_change_gbp):{FORMAT['GBP']}})`\n" \
+                    f"`{SYMBOL[args.convert]}{new_balance_con} ({balance_change_sign_con}{SYMBOL[args.convert]}{abs(balance_change_con):{FORMAT[args.convert]}})`"
+                )
                 open(POT_PATH, 'w+').write(f'{new_balance_gbp} GBP ({new_balance_con} {args.convert})')
         except:
             new_balance = f'{new_balance_gbp} GBP ({new_balance_con} {args.convert})'
