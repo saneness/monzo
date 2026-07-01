2f595a949555d26d7784a230bfe99a821be045e0
saneness <saneness@protonmail.com>
2026-07-01 17:06:17 +0100

    small update

--- rate.py
+++ rate.py
@@ -9,7 +9,7 @@ from forex_python.converter import CurrencyRates
 from config import *
 
 def args():
-    parser   = argparse.ArgumentParser(description="Wrapper for Forex exchange rates.")
+    parser   = argparse.ArgumentParser(description="Wrapper for currency rates.")
     parser.add_argument("-b", "--base", metavar="\b", default="GBP", help="Base currency to convert from. (default: GBP)")
     parser.add_argument("-c", "--convert", metavar="\b", default="JPY", help="Additional currency to convert to. (default: JPY)")
     args = parser.parse_args()
