913c8d58482c9f6b8ee3fe8b96dec433d7f83d82
saneness <saneness@protonmail.com>
2026-07-02 11:06:48 +0100

    add new rate provider

--- rate.py
+++ rate.py
@@ -3,6 +3,8 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import os
+import requests
 import traceback
 from forex_python.converter import CurrencyRates
 
@@ -15,12 +17,21 @@ def args():
     args = parser.parse_args()
     return args
 
+def forex(base, convert):
+    return CurrencyRates().get_rate(base, convert)
+
+def currencyapi(base, convert):
+    rates = requests.get(f'https://currencyapi.net/api/v2/rates?base=USD&output=json&key={CURRENCYAPI_KEY}').json()['rates']
+    return rates[convert] / rates[base]
+
 if __name__ == '__main__':
     args = args()
 
     try:
-        rate = CurrencyRates().get_rate(f'{args.base}', f'{args.convert}')
-        open(f'{RATE_PATH}', 'w+').write(f'{rate}')
+        rate = currencyapi(base=args.base, convert=args.convert)
+        if os.path.exists(RATE_PATH_ERROR):
+            os.remove(RATE_PATH_ERROR)
+        open(f'{RATE_PATH}', 'w+').write(f'{rate:.2f}')
     except Exception as e:
         traceback.print_exc()
         open(RATE_PATH_ERROR, 'w+').write('ERROR')
