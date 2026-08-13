a461c4fd76e42c9ff652d3a6538a6bcc40546285
saneness <saneness@protonmail.com>
2026-08-13 22:18:01 +0100

    update token values to the files

--- auth.py
+++ auth.py
@@ -20,6 +20,6 @@ if __name__ == '__main__':
     print(auth)
 
     AUTH_CODE = open('.auth_code').read().strip()
-    token = f'http --form POST "https://api.monzo.com/oauth2/token" "grant_type=authorization_code" "client_id={CLIENT_ID}" "client_secret={CLIENT_SECRET}" "redirect_uri={REDIRECT_URI}" "code={AUTH_CODE}"'
+    token = f'R=$(http --form POST "https://api.monzo.com/oauth2/token" "grant_type=authorization_code" "client_id={CLIENT_ID}" "client_secret={CLIENT_SECRET}" "redirect_uri={REDIRECT_URI}" "code={AUTH_CODE}"); jq -r \'"Bearer " + .access_token\' <<<"$R" > .access_token; jq -r \'.refresh_token\' <<<"$R" > .refresh_token'
     print("\nRe-login:")
     print(token)
